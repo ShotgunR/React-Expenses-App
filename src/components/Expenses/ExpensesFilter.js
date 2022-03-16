c57c781b62035d8937b4b4b664929f06f525c818
ShotgunR <onlywesley@hotmail.com>
2022-03-17 01:25:08 +0200

    Create

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -8,19 +8,23 @@ const ExpensesFilter = (props) => {
         props.onUpdateExpenseFilter(event.target.value)
     }
 
-  return (
-    <div className='expenses-filter'>
-      <div className='expenses-filter__control'>
-        <label>Filter by year</label>
-        <select value={props.selected} onChange={expenseFilterHandler}>
-          <option value='2022'>2022</option>
-          <option value='2021'>2021</option>
-          <option value='2020'>2020</option>
-          <option value='2019'>2019</option>
-        </select>
-      </div>
-    </div>
-  );
+    return ( <
+        div className = 'expenses-filter' >
+        <
+        div className = 'expenses-filter__control' >
+        <
+        label > Filter by year < /label> <
+        select value = { props.selected }
+        onChange = { expenseFilterHandler } >
+        <
+        option value = '2022' > 2022 < /option> <
+        option value = '2021' > 2021 < /option> <
+        option value = '2020' > 2020 < /option> <
+        option value = '2019' > 2019 < /option> <
+        /select> <
+        /div> <
+        /div>
+    );
 };
 
 export default ExpensesFilter;
